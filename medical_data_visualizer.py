15333ee0ae419f2d00a0df4c68b8552dd58b8ede
Hai Nguyen <thanhaing@yahoo.com>
2026-01-01 09:59:22 +0700

    final

---
+++ medical_data_visualizer.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+# 1
+os.chdir("D:/Python/boilerplate-medical-data-visualizer-main")
+df = pd.read_csv('medical_examination.csv')
+
+# 2
+df['overweight'] = np.where(df['weight']/((df['height']/100) ** 2) > 25, 1, 0)
+
+# 3
+
+
+# 4
+def draw_cat_plot():
+    # 5
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
+
+
+    # 6
+    df_group = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
+    
+
+    # 7
+    plot = sns.catplot(data=df_group, x='variable', y='total', hue='value', col='cardio', kind='bar')
+    
+
+    # 8
+    fig = plot.fig
+
+
+    # 9
+    fig.savefig('catplot.png')
+    return fig
+
+
+# 10
+def draw_heat_map():
+    # 11
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
+
+    # 12
+    corr = df_heat.corr()
+    
+    # 13
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+
+    # 14
+    fig, ax = plt.subplots(figsize=(12, 10))
+
+    # 15
+    sns.heatmap(
+        corr,
+        mask=mask,                   
+        annot=True,                
+        fmt='.1f',                   
+        cmap='coolwarm',             
+        vmin=-1, vmax=1,             
+        center=0,                  
+        square=True,               
+        linewidths=.5,             
+        cbar_kws={"shrink": .8},
+        ax=ax
+    )
+
+    #sns.heatmap(corr, mask=mask, cmap='coolwarm', vmin=-1, vmax=1, center=0, annot=True, fmt='.2f', square=True, linewidths=.5)
+    
+
+    # 16
+    fig.savefig('heatmap.png')
+    return fig
